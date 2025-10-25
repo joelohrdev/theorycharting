2d44d5b8416db087d3ae873b2f6add42cd1f17c8
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2025-10-24 21:08:59 -0500

    Disabled two factor auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Joe Lohr',
+            'email' => 'emailme@joelohr.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'is_admin' => true,
         ]);
     }
 }
