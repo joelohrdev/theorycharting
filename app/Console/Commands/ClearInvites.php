1c69810b3a8bb0a4f6e556a0958cd384ec98de4e
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2025-10-27 15:00:28 -0500

    Command to clear invites

---
+++ app/Console/Commands/ClearInvites.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Invitation;
+use Illuminate\Console\Command;
+
+class ClearInvites extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'invites:clear {--force : Skip confirmation prompt}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear all invitations from the database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $count = Invitation::count();
+
+        if ($count === 0) {
+            $this->info('No invitations to clear.');
+            return self::SUCCESS;
+        }
+
+        if (! $this->option('force')) {
+            if (! $this->confirm("Are you sure you want to delete {$count} invitation(s)?")) {
+                $this->info('Operation cancelled.');
+                return self::SUCCESS;
+            }
+        }
+
+        Invitation::query()->delete();
+
+        $this->info("Successfully deleted {$count} invitation(s).");
+
+        return self::SUCCESS;
+    }
+}
--- config/mail.php
+++ config/mail.php
@@ -51,6 +51,17 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'mailtrap' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
