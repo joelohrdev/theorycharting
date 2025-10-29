fbd0e188410c2ef39bbf9cdeb15b61f6b91d9a32
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2025-10-29 11:04:12 -0500

    Change to student navigation component

--- resources/views/components/student/navigation.blade.php
+++ resources/views/components/student/navigation.blade.php
@@ -1,5 +1,5 @@
 <flux:navbar>
-    <flux:navbar.item wire:navigate href="{{ route('student.index') }}">Active</flux:navbar.item>
-    <flux:navbar.item wire:navigate href="{{ route('student.invites') }}">Open Invites</flux:navbar.item>
-    <flux:navbar.item wire:navigate href="{{ route('student.deleted') }}">Deleted</flux:navbar.item>
+    <flux:navbar.item wire:navigate href="{{ route('student.index') }}" :current="request()->routeIs('student.index')">Active</flux:navbar.item>
+    <flux:navbar.item wire:navigate href="{{ route('student.invites') }}" :current="request()->routeIs('student.invites')">Open Invites</flux:navbar.item>
+    <flux:navbar.item wire:navigate href="{{ route('student.deleted') }}" :current="request()->routeIs('student.deleted')">Deleted</flux:navbar.item>
 </flux:navbar>
