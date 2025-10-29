314d5d3cac7193435017c1f2ac22ba8251ca49a8
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2025-10-29 10:44:27 -0500

    Updated name in sidebar

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Theory Charting</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold">Charting Theory</span>
 </div>
