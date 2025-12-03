e6f704e6bd9e13d448554fa713f467667eefb4d6
jlarm <117611704+jlarm@users.noreply.github.com>
2025-12-03 13:31:51 -0600

    Added Liters to O2 title

--- resources/views/livewire/patient/vitals-form.blade.php
+++ resources/views/livewire/patient/vitals-form.blade.php
@@ -475,7 +475,7 @@
     </tr>
 
     <tr class="border-b border-gray-100 dark:border-gray-800">
-        <td class="py-2 px-4 font-medium text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-950 sticky left-0 z-10 shadow-[2px_0_5px_-2px_rgba(0,0,0,0.1)] dark:shadow-[2px_0_5px_-2px_rgba(0,0,0,0.3)] border-r border-gray-200 dark:border-gray-700 text-left" style="width: 256px; min-width: 256px; max-width: 256px;">O2</td>
+        <td class="py-2 px-4 font-medium text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-950 sticky left-0 z-10 shadow-[2px_0_5px_-2px_rgba(0,0,0,0.1)] dark:shadow-[2px_0_5px_-2px_rgba(0,0,0,0.3)] border-r border-gray-200 dark:border-gray-700 text-left" style="width: 256px; min-width: 256px; max-width: 256px;">O2 Liters</td>
         @foreach($this->vitals as $vital)
             <td class="py-2 px-2 bg-white dark:bg-gray-950 text-center border-l border-r border-gray-200 dark:border-gray-700" style="width: 160px; min-width: 160px; max-width: 160px;">
                 <span @class(['text-xs text-gray-900 dark:text-gray-100', 'line-through' => $vital->trashed()])>{{ $vital->o2 ?? '-' }}</span>
